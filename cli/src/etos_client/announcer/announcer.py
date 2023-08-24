4d27bb3fc53b80d0677ce3b7d5590225ec3846d5
Tobias Persson <tobiaspn@axis.com>
2023-08-24 08:48:40 +0200

    Stop logging announcement events from ETOS

--- cli/src/etos_client/announcer/announcer.py
+++ cli/src/etos_client/announcer/announcer.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 """Announcer module."""
 import logging
-from etos_client.events.events import Events, TestSuite, Announcement
+from etos_client.events.events import Events, TestSuite
 
 
 class Announcer:  # pylint:disable=too-few-public-methods
@@ -23,28 +23,11 @@ class Announcer:  # pylint:disable=too-few-public-methods
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self):
-        """Init."""
-        self.__already_logged = []
-
-    def __log_announcements(self, announcements: list[Announcement]) -> None:
-        """Get latest new announcements."""
-        for announcement in announcements:
-            if announcement not in self.__already_logged:
-                self.__already_logged.append(announcement)
-                self.logger.info("%s: %s", announcement.heading, announcement.body)
-
     def __etos_state_information(self, test_suites: list[TestSuite]) -> str:
         """Generate text based on ETOS state."""
         message_template = (
-            "{announcement}\t"
-            "Started : {started_length}\t"
-            "Finished: {finished_length}\t"
+            "Started : {started_length}\t" "Finished: {finished_length}\t"
         )
-        try:
-            announcement = self.__already_logged[-1].body
-        except (KeyError, IndexError, TypeError):
-            announcement = ""
 
         started_length = 0
         finished_length = 0
@@ -57,7 +40,6 @@ class Announcer:  # pylint:disable=too-few-public-methods
         params = {
             "started_length": started_length,
             "finished_length": finished_length,
-            "announcement": announcement,
         }
         return message_template.format(**params)
 
@@ -67,6 +49,5 @@ class Announcer:  # pylint:disable=too-few-public-methods
             return
         if not events.activity.triggered:
             return
-        self.__log_announcements(events.announcements)
         if events.main_suites:
             self.logger.info(self.__etos_state_information(events.main_suites))
