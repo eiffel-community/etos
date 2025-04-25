8e39cc52a24ba90d5d20c1ee23786e738ef05a15
Tobias Persson <tobiaspn@axis.com>
2025-04-25 15:04:44 +0200

    Add e2e tests for the ETOS controller
    The test will deploy the controller and a fully functioning
    ETOS cluster and then deploy an ETOS testrun and wait for
    it to succeed.
    We only test the v1alpha version of ETOS at the moment but
    we could extend the actual test that we run in the testrun
    to start up a v0 testrun as well to verify that it works
    as expected.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -32,6 +32,22 @@ import (
 
 // namespace where the project is deployed in
 const namespace = "etos-system"
+const clusterNamespace = "etos-test"
+const clusterName = "cluster-sample"
+
+const clusterSample = "config/samples/etos_v1alpha1_cluster.yaml"
+const testRunSample = "config/samples/etos_v1alpha1_testrun.yaml"
+const iutProviderSample = "config/samples/etos_v1alpha1_iut_provider.yaml"
+const executionSpaceProviderSample = "config/samples/etos_v1alpha1_execution_space_provider.yaml"
+const logAreaProviderSample = "config/samples/etos_v1alpha1_log_area_provider.yaml"
+
+const executionSpaceProviderKustomization = "testdata/executionspace"
+const iutProviderKustomization = "testdata/iut"
+
+const goer = "testdata/goer.yaml"
+
+const artifactID = "268dd4db-93da-4232-a544-bf4c0fb26dac"
+const artifactIdentity = "pkg:testrun/etos/eiffel_community"
 
 // serviceAccountName created for the project
 const serviceAccountName = "etos-controller-manager"
@@ -60,10 +76,25 @@ var _ = Describe("Manager", Ordered, func() {
 		_, err = utils.Run(cmd)
 		Expect(err).NotTo(HaveOccurred(), "Failed to label namespace with restricted policy")
 
+		By("creating cluster namespace")
+		cmd = exec.Command("kubectl", "create", "ns", clusterNamespace)
+		_, err = utils.Run(cmd)
+		Expect(err).NotTo(HaveOccurred(), "Failed to create namespace")
+
 		By("installing CRDs")
 		cmd = exec.Command("make", "install")
 		_, err = utils.Run(cmd)
 		Expect(err).NotTo(HaveOccurred(), "Failed to install CRDs")
+
+		By("deploying the execution space provider")
+		cmd = exec.Command("kubectl", "create", "-k", executionSpaceProviderKustomization, "-n", clusterNamespace)
+		_, err = utils.Run(cmd)
+		Expect(err).NotTo(HaveOccurred(), "Failed to install execution space provider")
+
+		By("deploying the IUT provider")
+		cmd = exec.Command("kubectl", "create", "-k", iutProviderKustomization, "-n", clusterNamespace)
+		_, err = utils.Run(cmd)
+		Expect(err).NotTo(HaveOccurred(), "Failed to install IUT provider")
 
 		By("deploying the controller-manager")
 		cmd = exec.Command("make", "deploy", fmt.Sprintf("IMG=%s", projectImage))
@@ -74,8 +105,45 @@ var _ = Describe("Manager", Ordered, func() {
 	// After all tests have been executed, clean up by undeploying the controller, uninstalling CRDs,
 	// and deleting the namespace.
 	AfterAll(func() {
+		By("cleaning up the etos testrun")
+		cmd := exec.Command("kubectl", "delete", "--wait", "-n", clusterNamespace, "testrun/testrun-sample")
+		_, _ = utils.Run(cmd)
+		// This wait is necessary to make sure we clean up all resources before deleting the CRs that are
+		// being used. If we don't delete them the tests won't pass since we'll get stuck waiting for the
+		// namespace being deleted.
+		// TODO: We need a better way of waiting here.
+		time.Sleep(10 * time.Second)
+
+		By("cleaning up the etos cluster")
+		cmd = exec.Command("kubectl", "delete", "-n", clusterNamespace, "-f", clusterSample)
+		_, _ = utils.Run(cmd)
+
+		By("cleaning up the goer service")
+		cmd = exec.Command("kubectl", "delete", "-n", clusterNamespace, "-f", goer)
+		_, _ = utils.Run(cmd)
+
+		By("cleaning up the etos IUT provider")
+		cmd = exec.Command("kubectl", "delete", "-n", clusterNamespace, "-f", iutProviderSample)
+		_, _ = utils.Run(cmd)
+
+		By("cleaning up the etos log area provider")
+		cmd = exec.Command("kubectl", "delete", "-n", clusterNamespace, "-f", logAreaProviderSample)
+		_, _ = utils.Run(cmd)
+
+		By("cleaning up the etos execution space provider")
+		cmd = exec.Command("kubectl", "delete", "-n", clusterNamespace, "-f", executionSpaceProviderSample)
+		_, _ = utils.Run(cmd)
+
 		By("cleaning up the curl pod for metrics")
-		cmd := exec.Command("kubectl", "delete", "pod", "curl-metrics", "-n", namespace)
+		cmd = exec.Command("kubectl", "delete", "pod", "curl-metrics", "-n", namespace)
+		_, _ = utils.Run(cmd)
+
+		By("undeploying the IUT provider")
+		cmd = exec.Command("kubectl", "delete", "-k", iutProviderKustomization, "-n", clusterNamespace)
+		_, _ = utils.Run(cmd)
+
+		By("undeploying the execution space provider")
+		cmd = exec.Command("kubectl", "delete", "-k", executionSpaceProviderKustomization, "-n", clusterNamespace)
 		_, _ = utils.Run(cmd)
 
 		By("undeploying the controller-manager")
@@ -84,6 +152,10 @@ var _ = Describe("Manager", Ordered, func() {
 
 		By("uninstalling CRDs")
 		cmd = exec.Command("make", "uninstall")
+		_, _ = utils.Run(cmd)
+
+		By("removing cluster namespace")
+		cmd = exec.Command("kubectl", "delete", "ns", clusterNamespace)
 		_, _ = utils.Run(cmd)
 
 		By("removing manager namespace")
@@ -356,14 +428,267 @@ var _ = Describe("Manager", Ordered, func() {
 
 		// +kubebuilder:scaffold:e2e-webhooks-checks
 
-		// TODO: Customize the e2e test suite with scenarios specific to your project.
-		// Consider applying sample/CR(s) and check their status and/or verifying
-		// the reconciliation by using the metrics, i.e.:
-		// metricsOutput := getMetricsOutput()
-		// Expect(metricsOutput).To(ContainSubstring(
-		//    fmt.Sprintf(`controller_runtime_reconcile_total{controller="%s",result="success"} 1`,
-		//    strings.ToLower(<Kind>),
-		// ))
+		It("should be possible to deploy an ETOS execution space provider", func() {
+			By("deploying the sample Execution space provider")
+			cmd := exec.Command("kubectl", "create",
+				"-f", executionSpaceProviderSample,
+				"-n", clusterNamespace)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			By("checking the status field")
+			verifyExecutionSpace := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"provider", "execution-space-provider-sample", "-o",
+					"jsonpath={.status.conditions[?(@.type=='Available')].status}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("True"), "Incorrect Provider status")
+			}
+			Eventually(verifyExecutionSpace).Should(Succeed())
+		})
+		It("should be possible to deploy an ETOS IUT provider", func() {
+			By("deploying the sample IUT provider")
+			cmd := exec.Command("kubectl", "create",
+				"-f", iutProviderSample,
+				"-n", clusterNamespace)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			By("checking the status field")
+			verifyIut := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"provider", "iut-provider-sample", "-o", "jsonpath={.status.conditions[?(@.type=='Available')].status}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("True"), "Incorrect Provider status")
+			}
+			Eventually(verifyIut).Should(Succeed())
+		})
+
+		It("should be possible to deploy an ETOS log area provider", func() {
+			By("deploying the sample Log area provider")
+			cmd := exec.Command("kubectl", "create",
+				"-f", logAreaProviderSample,
+				"-n", clusterNamespace)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			verifyLogArea := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"provider", "log-area-provider-sample", "-o", "jsonpath={.status.conditions[?(@.type=='Available')].status}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("True"), "Incorrect Provider status")
+			}
+			Eventually(verifyLogArea).Should(Succeed())
+		})
+
+		It("should be possible to deploy an ETOS cluster", func() {
+			By("deploying the sample Cluster")
+			cmd := exec.Command("kubectl", "create",
+				"-f", clusterSample,
+				"-n", clusterNamespace)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should set cluster ready status true", func() {
+			By("checking Status for Cluster")
+			verifyClusterReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"cluster", clusterName, "-o", "jsonpath={.status.conditions[?(@.type=='Ready')].status}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("True"), "Incorrect ETOS cluster status")
+			}
+			Eventually(verifyClusterReady).Should(Succeed())
+		})
+		It("should deploy ETOS SSE", func() {
+			By("checking deployment status for ETOS SSE")
+			etosSSEName := fmt.Sprintf("%s-etos-sse", clusterName)
+			verifySSEReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", etosSSEName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS SSE deployment status")
+
+			}
+			Eventually(verifySSEReady).Should(Succeed())
+		})
+		It("should deploy ETOS LogArea", func() {
+			By("checking deployment status for ETOS LogArea")
+			etosLogAreaName := fmt.Sprintf("%s-etos-logarea", clusterName)
+			verifyLogAreaReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", etosLogAreaName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS LogArea deployment status")
+			}
+			Eventually(verifyLogAreaReady).Should(Succeed())
+		})
+		It("should deploy ETOS SuiteStarter", func() {
+			By("checking deployment status for ETOS SuiteStarter")
+			etosSuiteStarterName := fmt.Sprintf("%s-etos-suite-starter", clusterName)
+			verifySuiteStarterReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", etosSuiteStarterName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS SuiteStarter deployment status")
+
+			}
+			Eventually(verifySuiteStarterReady).Should(Succeed())
+		})
+		It("should deploy ETOS Messagebus", func() {
+			By("checking statefulset status for ETOS Messagebus")
+			etosMessageBusName := fmt.Sprintf("%s-messagebus", clusterName)
+			verifyMessageBusReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"statefulset", etosMessageBusName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS MessageBus statefulset status")
+			}
+			Eventually(verifyMessageBusReady).Should(Succeed())
+		})
+		It("should deploy ETOS RabbitMQ", func() {
+			By("checking statefulset status for ETOS RabbitMQ")
+			etosRabbitMQName := fmt.Sprintf("%s-rabbitmq", clusterName)
+			verifyRabbitMQReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"statefulset", etosRabbitMQName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS RabbitMQ statefulset status")
+			}
+			Eventually(verifyRabbitMQReady).Should(Succeed())
+		})
+		It("should deploy ETOS EventRepository", func() {
+			By("checking deployment status for ETOS EventRepository")
+			etosEventRepositoryName := fmt.Sprintf("%s-graphql", clusterName)
+			verifyEventRepositoryReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", etosEventRepositoryName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS GraphQL statefulset status")
+			}
+			Eventually(verifyEventRepositoryReady).Should(Succeed())
+		})
+		It("should deploy ETOS API", func() {
+			By("checking deployment status for ETOS API")
+			etosAPIName := fmt.Sprintf("%s-etos-api", clusterName)
+			verifyAPIReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", etosAPIName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect ETOS API deployment status")
+			}
+			Eventually(verifyAPIReady).Should(Succeed())
+		})
+		It("should deploy ETOS database", func() {
+			By("checking statefulset status for database")
+			etosDatabaseName := fmt.Sprintf("%s-etcd", clusterName)
+			verifyDatabaseReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"statefulset", etosDatabaseName, "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("3"), "Incorrect ETOS Database statefulset status")
+			}
+			Eventually(verifyDatabaseReady).Should(Succeed())
+		})
+
+		It("should deploy Goer for execution space provider", func() {
+			By("applying the yaml file")
+			cmd := exec.Command("kubectl", "create", "-n", clusterNamespace, "-f", goer)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred(), "Failed to create a goer deployment")
+			verifyGoerReady := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"deploy", "goer", "-o", "jsonpath={.status.readyReplicas}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("1"), "Incorrect Eiffel Goer deployment status")
+			}
+			Eventually(verifyGoerReady).Should(Succeed())
+		})
+
+		cmd := "from eiffel_graphql_api.graphql.db.database import insert_to_db;" +
+			"from eiffellib.events import EiffelArtifactCreatedEvent;" +
+			fmt.Sprintf("event = EiffelArtifactCreatedEvent(); event.meta.event_id = '%s';", artifactID) +
+			fmt.Sprintf("event.data.identity = '%s';insert_to_db(event);", artifactIdentity)
+		It("should be able to execute a v1alpha testrun", func() {
+			By("injecting a fake artifact to test")
+			cmd := exec.Command("kubectl", "run", "artifact-injector", "--restart=Never",
+				"--namespace", clusterNamespace,
+				"--image=ghcr.io/eiffel-community/eiffel-graphql-storage:latest",
+				"--overrides",
+				fmt.Sprintf(`{
+					"spec": {
+						"containers": [{
+							"name": "test",
+							"image": "ghcr.io/eiffel-community/eiffel-graphql-storage:latest",
+					    "envFrom": [{"secretRef": {"name": "cluster-sample-graphql"}}],
+							"command": ["python", "-c"],
+							"args": ["%s"],
+							"securityContext": {
+								"allowPrivilegeEscalation": false,
+								"capabilities": {
+									"drop": ["ALL"]
+								},
+								"runAsNonRoot": true,
+								"runAsUser": 1000,
+								"seccompProfile": {
+									"type": "RuntimeDefault"
+								}
+							}
+						}]
+					}
+				}`, cmd))
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred(), "Failed to inject artifact to eventrepository")
+
+			// TODO: Remove when fixed: https://github.com/eiffel-community/etos/issues/408
+			By("creating a generic encryption key")
+			cmd = exec.Command("kubectl", "create", "secret", "-n", clusterNamespace, "generic",
+				"etos-encryption-key", "--from-literal", "ETOS_ENCRYPTION_KEY=ZmgcW2Qz43KNJfIuF0vYCoPneViMVyObH4GR8R9JE4g=")
+			_, err = utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred(), "Failed to create an encryption key secret")
+
+			// Getting an EOF error every now and then from the environment-provider.
+
+			// TODO: This testrun could create a v0 testrun and wait for it to complete, as a way to test
+			// both ETOS versions.
+			By("creating a testrun")
+			cmd = exec.Command("kubectl", "create", "-n", clusterNamespace, "-f", testRunSample)
+			_, err = utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred(), "Failed to create a testrun")
+
+			By("checking the status field of the testrun")
+			verifyTestRun := func(g Gomega) {
+				cmd := exec.Command("kubectl", "get",
+					"testrun", "testrun-sample", "-o", "jsonpath={.status.verdict}",
+					"-n", clusterNamespace)
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("Passed"), "Incorrect TestRun status")
+			}
+			Eventually(verifyTestRun, "5m").Should(Succeed())
+		})
 	})
 })
 
