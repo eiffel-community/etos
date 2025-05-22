6bc777ae6be3db37e86edc5ddd88ff6fbe0ef89a
Fredrik Fristedt <fredjn@axis.com>
2025-05-22 16:39:26 +0200

    Remove parameter entirely

--- internal/etos/suitestarter/suitestarter.go
+++ internal/etos/suitestarter/suitestarter.go
@@ -670,8 +670,6 @@ func (r *ETOSSuiteStarterDeployment) suiteRunnerTemplate(templateName types.Name
                 value: /graveyard
               - name: OTEL_CONTEXT
                 value: {otel_context}
-			  - name: OTEL_EXPORTER_OTLP_ENDPOINT
-                value: {otel_exporter_otlp_endpoint}
               volumeMounts:
               - name: graveyard
                 mountPath: /graveyard
