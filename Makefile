a5b734eee90548e132d9c884b40f98b0fe574e71
Andrei Matveyeu <andreimu@axis.com>
2026-05-06 11:30:29 +0200

    Add E2E_REUSE option to skip infra setup in e2e tests (#621)

--- Makefile
+++ Makefile
@@ -86,8 +86,12 @@ setup-test-e2e: ## Set up a Kind cluster for e2e tests if it does not exist
 
 .PHONY: test-e2e
 test-e2e: setup-test-e2e manifests generate fmt vet ## Run the e2e tests. Expected an isolated environment using Kind.
-	KIND=$(KIND) KIND_CLUSTER=$(KIND_CLUSTER) go test -tags=e2e ./test/e2e/ -v -ginkgo.v -ginkgo.timeout=20m -timeout 0
-	$(MAKE) cleanup-test-e2e
+	E2E_REUSE=$(E2E_REUSE) KIND=$(KIND) KIND_CLUSTER=$(KIND_CLUSTER) go test -tags=e2e ./test/e2e/ -v -ginkgo.v -ginkgo.timeout=20m -timeout 0
+	@if [ "$(E2E_REUSE)" != "true" ]; then \
+		$(MAKE) cleanup-test-e2e; \
+	else \
+		echo "Skipping cluster cleanup (E2E_REUSE=true)"; \
+	fi
 
 .PHONY: cleanup-test-e2e
 cleanup-test-e2e: ## Tear down the Kind cluster used for e2e tests
--- test/e2e/e2e_testrun_test.go
+++ test/e2e/e2e_testrun_test.go
@@ -112,12 +112,15 @@ func VerifyETOSTestruns() {
 			}
 
 			By("cleaning up the artifact injector pod")
-			cmd := exec.Command("kubectl", "delete", "pod", "artifact-injector", "--namespace", clusterNamespace)
+			cmd := exec.Command("kubectl", "delete", "pod", "artifact-injector",
+				"--namespace", clusterNamespace, "--ignore-not-found")
 			_, _ = utils.Run(cmd)
 			By("cleaning up the ETOS testruns")
-			cmd = exec.Command("kubectl", "delete", "testrun", "testrun-sample", "-n", clusterNamespace)
+			cmd = exec.Command("kubectl", "delete", "testrun", "testrun-sample",
+				"-n", clusterNamespace, "--ignore-not-found")
 			_, _ = utils.Run(cmd)
-			cmd = exec.Command("kubectl", "delete", "testrun", "testrun-sample-multi-suite", "-n", clusterNamespace)
+			cmd = exec.Command("kubectl", "delete", "testrun", "testrun-sample-multi-suite",
+				"-n", clusterNamespace, "--ignore-not-found")
 			_, _ = utils.Run(cmd)
 
 			By("removing finalizers from EnvironmentRequests and Environments")
@@ -141,6 +144,14 @@ func VerifyETOSTestruns() {
 				}
 				cmd := exec.Command("kubectl", "patch", "environment", name, "--patch", "{\"metadata\": {\"finalizers\": []}}")
 				_, _ = utils.Run(cmd)
+			}
+
+			if reuseCluster {
+				_, _ = fmt.Fprintf(GinkgoWriter, "E2E_REUSE=true: preserving cluster resources\n")
+				// This wait is necessary to make sure we clean up all resources before
+				// the next test run.
+				time.Sleep(10 * time.Second)
+				return
 			}
 
 			// This wait is necessary to make sure we clean up all resources before deleting the CRs that are
@@ -198,6 +209,11 @@ func VerifyETOSTestruns() {
 			fmt.Sprintf("event = EiffelArtifactCreatedEvent(); event.meta.event_id = '%s';", artifactID) +
 			fmt.Sprintf("event.data.identity = '%s';insert_to_db(event);", artifactIdentity)
 		It("should prepare test environment", func() {
+			By("cleaning up any existing artifact injector pod")
+			cleanupCmd := exec.Command("kubectl", "delete", "pod", "artifact-injector",
+				"--namespace", clusterNamespace, "--ignore-not-found")
+			_, _ = utils.Run(cleanupCmd)
+
 			By("injecting a fake artifact to test")
 			cmd := exec.Command("kubectl", "run", "artifact-injector", "--restart=Never",
 				"--namespace", clusterNamespace,
@@ -231,7 +247,12 @@ func VerifyETOSTestruns() {
 			// TODO: Remove when fixed: https://github.com/eiffel-community/etos/issues/408
 			By("creating a generic encryption key")
 			cmd = exec.Command("kubectl", "create", "secret", "-n", clusterNamespace, "generic",
-				"etos-encryption-key", "--from-literal", "ETOS_ENCRYPTION_KEY=ZmgcW2Qz43KNJfIuF0vYCoPneViMVyObH4GR8R9JE4g=")
+				"etos-encryption-key", "--from-literal", "ETOS_ENCRYPTION_KEY=ZmgcW2Qz43KNJfIuF0vYCoPneViMVyObH4GR8R9JE4g=",
+				"--dry-run=client", "-o", "yaml")
+			secretYaml, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred(), "Failed to generate encryption key secret YAML")
+			cmd = exec.Command("kubectl", "apply", "-n", clusterNamespace, "-f", "-")
+			cmd.Stdin = strings.NewReader(secretYaml)
 			_, err = utils.Run(cmd)
 			Expect(err).NotTo(HaveOccurred(), "Failed to create an encryption key secret")
 		})
