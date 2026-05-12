c13d204acf085bde832d3aa97051fc3b2cddf6ec
Andrei Matveyeu <andreimu@axis.com>
2026-05-12 13:50:56 +0200

    Restore resource limits for environment-provider container
    Change-Id: I2cc9d2ed9e8d089d8ebe63e36fd24fed3973d8a6

--- internal/controller/environmentrequest_controller.go
+++ internal/controller/environmentrequest_controller.go
@@ -26,6 +26,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -750,6 +751,16 @@ func (r EnvironmentRequestReconciler) environmentProviderJob(ctx context.Context
 								fmt.Sprintf("-namespace=%s", environmentrequest.Namespace),
 								fmt.Sprintf("-environment-request=%s", environmentrequest.Name),
 								"-provider=environment-provider",
+							},
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("256Mi"),
+									corev1.ResourceCPU:    resource.MustParse("250m"),
+								},
+								Requests: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("128Mi"),
+									corev1.ResourceCPU:    resource.MustParse("100m"),
+								},
 							},
 						},
 					},
