45625b988f5ae2331fbf9a3818f671de961142b6
Tobias Persson <tobiaspn@axis.com>
2026-09-15 10:27:51 +0200

    Use controller-runtime logger

--- api/v1beta1/testrun_conversion.go
+++ api/v1beta1/testrun_conversion.go
@@ -18,18 +18,22 @@ package v1beta1
 
 import (
 	"fmt"
-	"log"
+
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
 	etosv1alpha1 "github.com/eiffel-community/etos/api/v1alpha1"
 )
 
+// log is for logging in this package.
+var log = logf.Log.WithName("testrun-conversion")
+
 // ConvertTo converts this TestRun (v1beta1) to the Hub version (v1alpha1).
 func (src *TestRun) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*etosv1alpha1.TestRun)
-	log.Printf("ConvertTo: Converting TestRun from Spoke version v1beta1 to Hub version v1alpha1;"+
-		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name)
+	log.Info(fmt.Sprintf("ConvertTo: Converting TestRun from Spoke version v1beta1 to Hub version v1alpha1;"+
+		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name))
 
 	dst.Spec.ID = src.Spec.ID
 	dst.Spec.Artifact = src.Spec.Artifact
@@ -96,8 +100,8 @@ func (src *TestRun) ConvertTo(dstRaw conversion.Hub) error {
 // ConvertFrom converts the Hub version (v1alpha1) to this TestRun (v1beta1).
 func (dst *TestRun) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*etosv1alpha1.TestRun)
-	log.Printf("ConvertFrom: Converting TestRun from Hub version v1alpha1 to Spoke version v1beta1;"+
-		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name)
+	log.Info(fmt.Sprintf("ConvertFrom: Converting TestRun from Hub version v1alpha1 to Spoke version v1beta1;"+
+		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name))
 
 	dst.Spec.ID = src.Spec.ID
 	dst.Spec.Name = src.Name
@@ -157,7 +161,7 @@ func (dst *TestRun) ConvertFrom(srcRaw conversion.Hub) error {
 // convertFrom converts the Image from the v1alpha1 Image to the v1beta1 Image.
 func (dst *Image) convertFrom(src *etosv1alpha1.Image) error {
 	if src == nil {
-		log.Printf("convertFrom: source Image is nil, skipping conversion to v1beta1")
+		log.Info("convertFrom: source Image is nil, skipping conversion to v1beta1")
 		return nil
 	}
 	dst.Image = src.Image
@@ -168,7 +172,7 @@ func (dst *Image) convertFrom(src *etosv1alpha1.Image) error {
 // convertTo converts the Image from the v1beta1 Image to the v1alpha1 Image.
 func (src *Image) convertTo(dst *etosv1alpha1.Image) error {
 	if src == nil {
-		log.Printf("convertTo: source Image is nil, skipping conversion to v1alpha1")
+		log.Info("convertTo: source Image is nil, skipping conversion to v1alpha1")
 		return nil
 	}
 	dst.Image = src.Image
@@ -179,7 +183,7 @@ func (src *Image) convertTo(dst *etosv1alpha1.Image) error {
 // convertFrom converts the TestRunner from the v1alpha1 TestRunner to the v1beta1 TestRunner.
 func (dst *TestRunner) convertFrom(src *etosv1alpha1.TestRunner) error {
 	if src == nil {
-		log.Printf("convertFrom: source TestRunner is nil, skipping conversion to v1beta1")
+		log.Info("convertFrom: source TestRunner is nil, skipping conversion to v1beta1")
 		return nil
 	}
 	dst.Version = src.Version
@@ -189,7 +193,7 @@ func (dst *TestRunner) convertFrom(src *etosv1alpha1.TestRunner) error {
 // convertTo converts the TestRunner from the v1beta1 TestRunner to the v1alpha1 TestRunner.
 func (src *TestRunner) convertTo(dst *etosv1alpha1.TestRunner) error {
 	if src == nil {
-		log.Printf("convertTo: source TestRunner is nil, skipping conversion to v1alpha1")
+		log.Info("convertTo: source TestRunner is nil, skipping conversion to v1alpha1")
 		return nil
 	}
 	dst.Version = src.Version
